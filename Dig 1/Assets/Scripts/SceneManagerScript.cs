76b49e1d8f0d3c7a3dc9f981cc2ba28dcbb1283b
Molly Claesson <mollyclaesson09@gmail.com>
2026-01-29 10:19:20 +0100

    SceneManager Load Game

---
+++ Dig 1/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+   
+
+    public void LoadNewGame()
+    {
+      SceneManager.LoadScene(1);
+    }
+
+  //  public void LoadNextScene()
+    //{
+       // int currentSene = SceneManager.GetActiveScene().buildIndex;
+      //  SceneManager.LoadScene(currentScene + 1);
+    //}
+}
