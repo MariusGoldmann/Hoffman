17f248d36374031c5fc4743b7be75fc042dce0be
MariusGoldmann <mariuspancakez@gmail.com>
2026-01-29 09:21:35 +0100

    Enemy work in progress commit

--- Dig 1/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Dig 1/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] float idleMoveSpeed = 1f;
-    [SerializeField] float waypointDistance = 0.2f;
+    [SerializeField] float idleMoveSpeed = 0.5f;
+    [SerializeField] float chaseMoveSpeed = 1f;
+    [SerializeField] float waypointDistance = 0.5f;
     
     [SerializeField] Transform[] waypoints;
 
     [Header ("Debug")]
     [SerializeField] int waypointIndex;
+    bool inRange;
+    bool facingRight;
 
     Rigidbody2D rigidBody;
     Animator animator;
@@ -18,7 +21,7 @@ public class EnemyMovement : MonoBehaviour
     {
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
-        state = GetComponent <EnemyState>();
+        state = GetComponentInChildren<EnemyState>();
     }
 
     void Update()
@@ -52,15 +55,27 @@ public class EnemyMovement : MonoBehaviour
         else if (Vector2.Distance(transform.position, waypoints[waypointIndex].position) < waypointDistance)
         {
             waypointIndex++;
+            facingRight = !facingRight;
         }
         else 
         {
-            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].position, idleMoveSpeed);
+            rigidBody.MovePosition(Vector2.MoveTowards(transform.position, waypoints[waypointIndex].position, idleMoveSpeed));
         }
     }
 
     void ChasePlayer()
     {
+        rigidBody.linearVelocity = new Vector2(state.GetPlayerDirection().x*chaseMoveSpeed, rigidBody.linearVelocityY);
+        Debug.Log("chase");
+    }
 
+    public void TurnAround()
+    {
+
+    }
+
+    public bool GetFacingRight()
+    {
+        return facingRight;
     }
 }
