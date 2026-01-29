506137d20bf40d0e5d93ac82bff1713ad6a15137
Raymondaderman <raymond.aderman@gmail.com>
2026-01-29 15:04:12 +0100

    No idea

--- Dig 1/Assets/Scripts/Player/PlayerHealth.cs
+++ Dig 1/Assets/Scripts/Player/PlayerHealth.cs
@@ -2,10 +2,5 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] int playerHealth = 10;
 
-    public int GetPlayerHealth()
-    {
-        return playerHealth;
-    }
 }
