4f381a3e07f6324c41e6f829aaf05d1a2f7b0ffa
Molly Claesson <mollyclaesson09@gmail.com>
2026-01-30 14:11:30 +0100

    Pause and Ability Tab, not done yet.

--- Dig 1/Assets/Scripts/PauseManager.cs
+++ Dig 1/Assets/Scripts/PauseManager.cs
@@ -5,14 +5,18 @@ using UnityEngine.InputSystem;
 public class PauseManager : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private GameObject abilityTabUI;
 
     InputAction pauseAction;
+    InputAction abilityTabAction; 
 
     void Start()
     {
         pauseMenuUI.SetActive(false);
+        abilityTabUI.SetActive(false);
 
         pauseAction = InputSystem.actions.FindAction("Pause");
+        abilityTabAction = InputSystem.actions.FindAction("AbilityTab");
     }
 
     void Update()
@@ -22,11 +26,18 @@ public class PauseManager : MonoBehaviour
             pauseMenuUI.SetActive(true); 
             Time.timeScale = 0;
         }
+
+        if (abilityTabAction.WasPerformedThisFrame())
+        {
+            abilityTabUI.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 
     public void ResumeGame()
     {
         pauseMenuUI.SetActive(false);
+        abilityTabUI.SetActive(false);
         TimeStart();
     }
 
