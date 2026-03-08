bb399156cd8670042f78e6e277fe12d809bd986c
Raymondaderman <raymond.aderman@gmail.com>
2026-03-08 18:56:14 +0100

    Damage flash and major boomerang tweaks.
    I added a damage flash using shader graphics. For the boomerang i am now using a animationcurve when throwing it and also made a new script for it that handles all collissions. If the boomerang collides with a wall it will immidiatly return to the player, and same goes for enemy collision.

--- Dig 1/Assets/Scripts/Player/PlayerCombat.cs
+++ Dig 1/Assets/Scripts/Player/PlayerCombat.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using static PlayerMovement;
 
 public class PlayerCombat : MonoBehaviour
@@ -25,9 +26,12 @@ public class PlayerCombat : MonoBehaviour
     [SerializeField] int boomerangDamage = 5;
     [SerializeField] int boomerangCooldown = 5;
     [SerializeField] float boomerangTimer;
-    [SerializeField] float boomerangLengh;
     [SerializeField] float boomerangForce;
     [SerializeField] float boomerangReturnForce;
+
+    [SerializeField] bool earlyReceiving;
+
+    [SerializeField] AnimationCurve boomerangAnimationCurve;
 
     // Private variables
     Coroutine boomerangSpawnerCoroutine;
@@ -37,7 +41,7 @@ public class PlayerCombat : MonoBehaviour
     [SerializeField] PickUpScript pickUpScript;
 
     // Component references
-    [SerializeField] GameObject boomerangPrefab;
+    [SerializeField] GameObject boomerangPrefab; // drag in inspector
     Animator animator;
 
     void Awake()
@@ -57,51 +61,50 @@ public class PlayerCombat : MonoBehaviour
     {
         Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, enemyLayer);
 
+        animator.SetTrigger(animation);
+
         if (enemies != null)
         {
             foreach (Collider2D enemy in enemies)
             {
                 enemy.GetComponent<EnemyHealth>().ChangeHealth(-damage);
             }
-
-            animator.SetTrigger(animation);
         }
     }
 
     IEnumerator BoomerangSpawner()
     {
         Vector3 spawnPosition = new Vector3(transform.position.x + 2 * playerMovement.GetFacingDirection(), transform.position.y, transform.position.z);
-
-
         GameObject boomerang = Instantiate(boomerangPrefab, spawnPosition, Quaternion.identity);
         
         Rigidbody2D boomerangRB = boomerang.GetComponent<Rigidbody2D>();
 
-        float boomerangSpeed = boomerangForce;
+        earlyReceiving = false;
+
+        float timer = 0;
+        float duration = 1;
+        float boomerangSpeed;
         float boomerangReturnSpeed = boomerangReturnForce;
-        int boomerangDirection = playerMovement.GetFacingDirection();
+        int boomerangDirection = playerMovement.GetFacingDirection(); //Where the player is facing
         
-        while (boomerangSpeed > 1)
+        while (timer < duration && !earlyReceiving)
         {
-            boomerangRB.linearVelocity = new Vector2(boomerangDirection *  boomerangSpeed, boomerangRB.linearVelocityY);
-            boomerangSpeed = Mathf.Lerp(boomerangSpeed, 0, 1 * Time.deltaTime);
+            timer += Time.deltaTime;
+            boomerangSpeed = boomerangForce * boomerangAnimationCurve.Evaluate(timer / duration);
 
-            Debug.Log(boomerangSpeed);
+            boomerangRB.linearVelocity = new Vector2(boomerangDirection * boomerangSpeed, boomerangRB.linearVelocity.y);
+
             yield return null;
         }
-
-       // yield return new WaitForSeconds(boomerangLengh);
         
-        while (boomerangRB != null && Vector2.Distance(boomerang.transform.position, transform.position) > 0.1f)
+        while (boomerang != null && Vector2.Distance(boomerang.transform.position, transform.position) > 0.1f || earlyReceiving)
         {
-            boomerang.transform.position = Vector2.MoveTowards(boomerang.transform.position , transform.position, boomerangReturnSpeed * Time.deltaTime);
             boomerangReturnSpeed += 50 * Time.deltaTime;
+            boomerang.transform.position = Vector2.MoveTowards(boomerang.transform.position , transform.position, boomerangReturnSpeed * Time.deltaTime);
 
-
+            earlyReceiving = true;
             yield return null;
         }
-
-        boomerangSpawnerCoroutine = null;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -110,7 +113,9 @@ public class PlayerCombat : MonoBehaviour
         {
             Debug.Log("Boomerang picked up");
             GameObject boomerang = collision.gameObject;
+            earlyReceiving = false;
 
+            boomerangSpawnerCoroutine = null;
             Destroy(boomerang);
 
             boomerangTimer = boomerangCooldown;
@@ -121,9 +126,9 @@ public class PlayerCombat : MonoBehaviour
     {
         if (slashbutton.isPressed && slashTimer <= 0 && pickUpScript.GetHasLeg())
         {
+            slashTimer = slashCooldown;
             MeleeAttack(slashDamage, "Slash");
             Debug.Log("Slash");
-            slashTimer = slashCooldown;
         }
     }
 
@@ -158,6 +163,11 @@ public class PlayerCombat : MonoBehaviour
     public int GetBoomerangDamage()
     {
         return boomerangDamage;
+    }
+
+    public bool GetEarlyReceiving(bool value)
+    {
+        return earlyReceiving = value;
     }
 
     void OnDrawGizmos() // Attack radius debug
