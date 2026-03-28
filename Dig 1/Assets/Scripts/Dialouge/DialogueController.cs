58c985db7cb0b74e721a70a4dfbd678859ce809d
Raymondaderman <raymond.aderman@gmail.com>
2026-03-28 14:05:11 +0100

    Complete Dialogue system.

---
+++ Dig 1/Assets/Scripts/Dialouge/DialogueController.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI NPCNameText;
+    [SerializeField] TextMeshProUGUI NPCDialogueText;
+
+    [SerializeField] float typeSpeed = 5f;
+    [SerializeField] float MaxTypeTime = 0.1f;
+
+    Queue<string> paragraphs = new Queue<string>();
+
+    bool conversationEnded;
+    bool isTyping;
+
+    string paragraph;
+
+    Coroutine typewriterCoroutine;
+
+    public void DisplayNextParagraph(DialogueText dialogueText)
+    {
+        if (paragraphs.Count == 0)
+        {
+            if (!conversationEnded)
+            {
+                StartConversation(dialogueText);
+            }
+            else if (conversationEnded && !isTyping)
+            {
+                EndConversation();
+                return;
+            }
+        }
+
+        if (!isTyping)
+        {
+            paragraph = paragraphs.Dequeue();
+
+            typewriterCoroutine = StartCoroutine(Typewriter(paragraph));
+        }
+        else
+        {
+            FinishParagraphEarly();
+        }
+
+        NPCDialogueText.text = paragraph;
+
+        if (paragraphs.Count == 0)
+        {
+            conversationEnded = true;
+        }
+    }
+
+    void StartConversation(DialogueText dialogueText)
+    {
+        if (!gameObject.activeSelf)
+        {
+            gameObject.SetActive(true);
+        }
+
+        NPCNameText.text = dialogueText.npcName;
+
+        for (int i = 0; i < dialogueText.paragraphs.Length; i++)
+        {
+            paragraphs.Enqueue(dialogueText.paragraphs[i]);
+        }
+    }
+    void EndConversation()
+    {
+        conversationEnded = false;
+
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator Typewriter(string paragraph)
+    {
+        isTyping = true;
+
+        int maxVisibleChars = 0;
+
+        NPCDialogueText.text = paragraph;
+        NPCDialogueText.maxVisibleCharacters = maxVisibleChars;
+
+        foreach (char c in paragraph.ToCharArray())
+        {
+
+            maxVisibleChars++;
+            NPCDialogueText.maxVisibleCharacters = maxVisibleChars;
+
+            yield return new WaitForSeconds(MaxTypeTime / typeSpeed);
+        }
+
+        isTyping = false;
+    }
+
+    void FinishParagraphEarly()
+    {
+        StopCoroutine(typewriterCoroutine);
+
+        NPCDialogueText.maxVisibleCharacters = paragraph.Length;
+
+        isTyping = false;
+    }
+}
---
+++ Dig 1/Assets/Scripts/PlayerSoundEffectManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerSoundEffectManager : MonoBehaviour
+{
+    [SerializeField] AudioClip[] soundEffectList;
+    static PlayerSoundEffectManager instance;
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(SoundType sound, float volume = 1)
+    {
+        instance.audioSource.PlayOneShot(instance.soundEffectList[(int)sound], volume);
+    }
+
+    public enum SoundType
+    {
+        WALK,
+        RUN,
+        SLASH,
+        KICK
+    }
+
+}
