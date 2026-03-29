30b54256f8e4d4644de6c3ec57bbd19ade9d88f3
Raymondaderman <raymond.aderman@gmail.com>
2026-03-29 13:25:24 +0200

    Random stuff

--- Dig 1/Assets/Scripts/Jumppad.cs
+++ Dig 1/Assets/Scripts/Jumppad.cs
@@ -15,7 +15,6 @@ public class Jumppad : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<Rigidbody2D>().linearVelocityY = 0;
-            Debug.Log("Jumppad");
             animator.SetTrigger("Contact");
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumppadForce, ForceMode2D.Impulse);  
         }
