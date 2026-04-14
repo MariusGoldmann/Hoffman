c904d059f334beb4a5513ef87cff791db02959ae
Raymondaderman <raymond.aderman@gmail.com>
2026-04-14 09:56:51 +0200

    Gaia scene complete

--- Dig 1/Assets/Scripts/Player/PlayerHealth.cs
+++ Dig 1/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,7 +1,5 @@
 using Cinemachine;
 using System.Collections;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
