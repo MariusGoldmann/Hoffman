62a415b4bf8f09bdf363f034db10676697c7ac91
Reijii 3 <mollyclaesson09@gmail.com>
2026-04-17 16:00:22 +0200

    Pause Button Animator

--- Dig 1/Assets/Scripts/ButtonScript.cs
+++ Dig 1/Assets/Scripts/ButtonScript.cs
@@ -1,87 +1,26 @@
-using Unity.VisualScripting;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.UI;
-using static UnityEngine.Timeline.DirectorControlPlayable;
 
+using UnityEngine;
+using UnityEngine.UI;
 public class ButtonScript : MonoBehaviour
 {
     private Button buttonElement;
 
-    //void Awake()
-    //{
-    //    buttonPressed = false;
-    //}
+    PauseManager pauseManager;
 
     public void Start()
     {
         buttonElement = GetComponent<Button>();
         buttonElement.onClick.AddListener(OnButtonPressed);
+        pauseManager = FindAnyObjectByType<PauseManager>();
     }
-
-    //private void OnEnable()
-    //{
-    //    transform.localScale = startSize;
-    //}
-
-    //public void OnPointerDown(PointerEventData eventData)
-    //{
-    //    buttonPressed = true;
-    //}
-
-    //public void OnPointerUp(PointerEventData eventData)
-    //{
-    //    buttonPressed = false;
-    //    transform.localScale = startSize;
-    //}
 
     public void OnButtonPressed()
     {
         PlayerSoundFXManager.instance.PlaySound(PlayerSoundFXManager.SoundType.BUTTON, 1);
     }
 
-    
-
-
-    //private void Update()
-    //{
-
-    //    if (buttonPressed == true)
-    //    {
-    //        transform.localScale = desiredSize;
-    //        PlayerSoundFXManager.instance.PlaySound(PlayerSoundFXManager.SoundType.BUTTON, 1);
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("Update loop");
-    //        transform.localScale = startSize;
-    //    }
-
-        /* while (buttonPressed && transform.localScale.x < desiredSize.x)
-         {
-             transform.localScale = Vector3.Lerp(transform.localScale, desiredSize, 10 * Time.deltaTime);
-             break;
-         }
-         while (!buttonPressed)
-         {
-             transform.localScale = Vector3.Lerp(transform.localScale, desiredSize, 10 * Time.deltaTime);
-             break;
-         }*/
-
-
-        /*while (buttonPressed)
-     {
-        transform.localScale = Vector3.Lerp(transform.localScale, desiredSize, scaleSpeed * Time.deltaTime);
-         break;
-     }
-     while (!buttonPressed)
-     {
-             transform.localScale = Vector3.Lerp(transform.localScale, startSize, scaleSpeed * Time.deltaTime);
-            break;
-      }*/
-    //}
-
-
-
+    public void ResumeGame()
+    {
+        pauseManager.ResumeGame();
+    }
 }
