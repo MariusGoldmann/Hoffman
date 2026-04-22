04ff135177ebd52874e8c7521d5a017474acd0e3
Raymondaderman <raymond.aderman@gmail.com>
2026-04-22 20:12:32 +0200

    Clean

--- Dig 1/Assets/Scripts/Enemy/Dragon Enemy/Dragon.cs
+++ Dig 1/Assets/Scripts/Enemy/Dragon Enemy/Dragon.cs
@@ -1,198 +1,174 @@
 using System.Collections;
 using UnityEngine;
 
-public class Dragon : MonoBehaviour
-{
-    [Header("General settings")]
-    [SerializeField] float idleMoveSpeed;
-    [SerializeField] float recoilForce;
-    [SerializeField] float detectionRadius;
+public class Dragon : MonoBehaviour {
+	[Header("General settings")]
+	[SerializeField] float idleMoveSpeed;
+	[SerializeField] float recoilForce;
+	[SerializeField] float detectionRadius;
 
-    [Header("Attack settings")]
-    [SerializeField] int attackCooldown;
-    [SerializeField] int projectileSpeed;
-    [SerializeField] int anticipationTime;
-    [SerializeField] Transform attackPoint;
-    [SerializeField] ObjectPooling fireProjectilePool;
+	[Header("Attack settings")]
+	[SerializeField] int attackCooldown;
+	[SerializeField] int           projectileSpeed;
+	[SerializeField] int           anticipationTime;
+	[SerializeField] Transform     attackPoint;
+	[SerializeField] ObjectPooling fireProjectilePool;
 
-    [Header("Death settings")]
-    [SerializeField] float deathTime;
-    [SerializeField] bool isDead = false;
+	[Header("Death settings")]
+	[SerializeField] float deathTime;
+	[SerializeField] bool isDead = false;
 
-    [Header("Raycast/Collider settings")]
-    [SerializeField] Transform groundCheck;
-    [SerializeField] Transform wallCheck;
-    [SerializeField] GameObject player;
-    [SerializeField] float horizontalDetectRange;
-    [SerializeField] float verticalDetectRange;
-    [SerializeField] float groundCheckDistance;
-    [SerializeField] float wallCheckDistance;
+	[Header("Raycast/Collider settings")]
+	[SerializeField] Transform groundCheck;
+	[SerializeField] Transform  wallCheck;
+	[SerializeField] GameObject player;
+	[SerializeField] float      horizontalDetectRange;
+	[SerializeField] float      verticalDetectRange;
+	[SerializeField] float      groundCheckDistance;
+	[SerializeField] float      wallCheckDistance;
 
-    //Private variables
-    float cooldownTimer;
-    int facingDirection = 1;
-    LayerMask groundLayer;
+	//Private variables
+	float     cooldownTimer;
+	int       facingDirection = 1;
+	LayerMask groundLayer;
 
-    //Script/Component references
-    KnockbackScript dragonKnockBackScript;
-    EnemyHealth enemyHealth;
-    Rigidbody2D dragonRB;
+	//Script/Component references
+	KnockbackScript dragonKnockBackScript;
+	EnemyHealth     enemyHealth;
+	Rigidbody2D     dragonRB;
 
-    Coroutine rangedAttackCoroutine;
-    Coroutine deathCoroutine;
+	Coroutine rangedAttackCoroutine;
+	Coroutine deathCoroutine;
 
-    void Awake()
-    {
-        groundLayer = LayerMask.GetMask("Ground");
-        dragonKnockBackScript = GetComponent<KnockbackScript>();
-        enemyHealth = GetComponent<EnemyHealth>();
-        dragonRB = GetComponent<Rigidbody2D>();
-    }
+	void Awake() {
+		groundLayer           = LayerMask.GetMask("Ground");
+		dragonKnockBackScript = GetComponent<KnockbackScript>();
+		enemyHealth           = GetComponent<EnemyHealth>();
+		dragonRB              = GetComponent<Rigidbody2D>();
+	}
 
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
 
-    void FixedUpdate()
-    {
-        Patrol();
-        Flip();
-        HandleCooldown();
-        DeathSequence();
-    }
+	void FixedUpdate() {
+		Patrol();
+		Flip();
+		HandleCooldown();
+		DeathSequence();
+	}
 
-    void Patrol()
-    {
-        if (!IsPlayerDetected() && !isDead)
-        {
-            dragonRB.linearVelocityX = idleMoveSpeed * facingDirection;
-            rangedAttackCoroutine = null;
-        }
-        else
-        {
-            dragonRB.linearVelocity = new(dragonRB.linearVelocity.x, dragonRB.linearVelocity.y);
-            if (cooldownTimer <= 0 && !isDead)
-            {
-                rangedAttackCoroutine = StartCoroutine(RangedAttack());
-            }
-        }
-    }
+	void Patrol() {
+		if (!IsPlayerDetected() && !isDead) {
+			dragonRB.linearVelocityX = idleMoveSpeed * facingDirection;
+			rangedAttackCoroutine    = null;
+		} else {
+			dragonRB.linearVelocity = new(dragonRB.linearVelocity.x, dragonRB.linearVelocity.y);
+			if (cooldownTimer <= 0 && !isDead) {
+				rangedAttackCoroutine = StartCoroutine(RangedAttack());
+			}
+		}
+	}
 
-    IEnumerator RangedAttack()
-    {
-        cooldownTimer = attackCooldown;
-        float anticipationTimer = 0;
+	IEnumerator RangedAttack() {
+		cooldownTimer = attackCooldown;
+		float anticipationTimer = 0;
 
-        while (anticipationTime > anticipationTimer)
-        {
-            anticipationTimer += Time.deltaTime;
-            yield return null;
-        }
+		while (anticipationTime > anticipationTimer) {
+			anticipationTimer += Time.deltaTime;
+			yield return null;
+		}
 
-        if (PlayerTarget() != null)
-        {
-            PlayerSoundFXManager.instance.PlaySound(PlayerSoundFXManager.SoundType.FIREBALL, 1);
-            dragonRB.linearVelocity = new Vector2((recoilForce) * facingDirection * -1, recoilForce);
-            Vector2 fireDirection = (PlayerTarget().position - attackPoint.position).normalized;
-            float angle = Mathf.Atan2(fireDirection.y, fireDirection.x) * Mathf.Rad2Deg;
+		if (PlayerTarget() != null) {
+			PlayerSoundFXManager.instance.PlaySound(PlayerSoundFXManager.SoundType.FIREBALL, 1);
+			dragonRB.linearVelocity = new Vector2((recoilForce) * facingDirection * -1, recoilForce);
+			Vector2 fireDirection = (PlayerTarget().position - attackPoint.position).normalized;
+			float   angle         = Mathf.Atan2(fireDirection.y, fireDirection.x) * Mathf.Rad2Deg;
 
-            Quaternion rotation = Quaternion.Euler(0, 0, angle);
-            GameObject projectile = fireProjectilePool.GetObject(attackPoint.position, rotation);
-            Rigidbody2D projectileRB = projectile.GetComponent<Rigidbody2D>();
-            projectileRB.linearVelocity = fireDirection * projectileSpeed;
-        }
-        rangedAttackCoroutine = null;
-    }
+			Quaternion  rotation     = Quaternion.Euler(0, 0, angle);
+			GameObject  projectile   = fireProjectilePool.GetObject(attackPoint.position, rotation);
+			Rigidbody2D projectileRB = projectile.GetComponent<Rigidbody2D>();
+			projectileRB.linearVelocity = fireDirection * projectileSpeed;
+		}
 
-    void DeathSequence()
-    {
-        if (enemyHealth.GetHealth() <= 0)
-        {
-            isDead = true;
-            dragonRB.linearVelocity = Vector2.zero;
-            if (deathCoroutine != null)
-            {
-                return;
-            }
-            deathCoroutine = StartCoroutine(DeathCoroutine());
-        }
-    }
+		rangedAttackCoroutine = null;
+	}
 
-    IEnumerator DeathCoroutine()
-    {
-        Debug.Log("Hej");
-        yield return new WaitForSeconds(3);
-        Destroy(gameObject);
-    }
+	void DeathSequence() {
+		if (enemyHealth.GetHealth() <= 0) {
+			isDead                  = true;
+			dragonRB.linearVelocity = Vector2.zero;
+			if (deathCoroutine != null) {
+				return;
+			}
 
-    void Flip()
-    {
-        if (IsAtEdge() && !isDead || IsAtWall() && !isDead)
-        {
-            facingDirection = facingDirection * -1;
-        }
+			deathCoroutine = StartCoroutine(DeathCoroutine());
+		}
+	}
 
-        if (PlayerTarget() != null && !isDead)
-        {
-            facingDirection = (int)Mathf.Sign(PlayerTarget().position.x - transform.position.x);
-        }
-        transform.localScale = new Vector2(facingDirection, 1);
-    }
+	IEnumerator DeathCoroutine() {
+		Debug.Log("Hej");
+		yield return new WaitForSeconds(3);
+		Destroy(gameObject);
+	}
 
-    void HandleCooldown()
-    {
-        cooldownTimer -= Time.deltaTime;
-    }
+	void Flip() {
+		if (IsAtEdge() && !isDead || IsAtWall() && !isDead) {
+			facingDirection = facingDirection * -1;
+		}
 
-    bool IsAtEdge()
-    {
-        return !Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayer);
-    }
+		if (PlayerTarget() != null && !isDead) {
+			facingDirection = (int)Mathf.Sign(PlayerTarget().position.x - transform.position.x);
+		}
 
-    bool IsAtWall()
-    {
-        return Physics2D.Raycast(wallCheck.position, Vector2.up, wallCheckDistance, groundLayer);
-    }
+		transform.localScale = new Vector2(facingDirection, 1);
+	}
 
-    Transform PlayerTarget()
-    {
-        if (Vector2.Distance(transform.position, player.transform.position) < detectionRadius)
-        {
-            var hit = Physics2D.Linecast(transform.position, player.transform.position, ~LayerMask.GetMask("Enemy", "FireProjectile"));
+	void HandleCooldown() {
+		cooldownTimer -= Time.deltaTime;
+	}
 
-            if (hit.collider.gameObject.CompareTag("Player"))
-            {
-                return hit.transform;
-            }
-        }
-        return null;
-    }
+	bool IsAtEdge() {
+		return !Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayer);
+	}
 
-    bool IsPlayerDetected()
-    {
-        if (Vector2.Distance(transform.position, player.transform.position) < detectionRadius)
-        {
-            var hit = Physics2D.Linecast(transform.position, player.transform.position, ~LayerMask.GetMask("Enemy", "FireProjectile"));
+	bool IsAtWall() {
+		return Physics2D.Raycast(wallCheck.position, Vector2.up, wallCheckDistance, groundLayer);
+	}
 
-            if (hit.collider.gameObject.CompareTag("Player")) return true;
+	Transform PlayerTarget() {
+		if (Vector2.Distance(transform.position, player.transform.position) < detectionRadius) {
+			var hit = Physics2D.Linecast(transform.position, player.transform.position,
+			                             ~LayerMask.GetMask("Enemy", "FireProjectile"));
 
-        }
-        return false;
-    }
+			if (hit.collider.gameObject.CompareTag("Player")) {
+				return hit.transform;
+			}
+		}
 
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, player.transform.position);
-        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+		return null;
+	}
 
-        Gizmos.color = Color.green;
-        Gizmos.DrawRay(groundCheck.position, Vector2.down * groundCheckDistance);
+	bool IsPlayerDetected() {
+		if (Vector2.Distance(transform.position, player.transform.position) < detectionRadius) {
+			var hit = Physics2D.Linecast(transform.position, player.transform.position,
+			                             ~LayerMask.GetMask("Enemy", "FireProjectile"));
 
-        Gizmos.color = Color.blue;
-        Gizmos.DrawRay(wallCheck.position, Vector2.up * wallCheckDistance);
-    }
+			if (hit.collider.gameObject.CompareTag("Player")) return true;
+		}
 
+		return false;
+	}
 
+	void OnDrawGizmos() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(transform.position, player.transform.position);
+		Gizmos.DrawWireSphere(transform.position, detectionRadius);
+
+		Gizmos.color = Color.green;
+		Gizmos.DrawRay(groundCheck.position, Vector2.down * groundCheckDistance);
+
+		Gizmos.color = Color.blue;
+		Gizmos.DrawRay(wallCheck.position, Vector2.up * wallCheckDistance);
+	}
 }
