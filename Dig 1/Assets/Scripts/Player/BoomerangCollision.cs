94f308df792851759a2b732c7563acb8d59d880d
Raymondaderman <raymond.aderman@gmail.com>
2026-04-26 15:51:15 +0200

    Door and lever, dragon, map, abikity ui

--- Dig 1/Assets/Scripts/Player/BoomerangCollision.cs
+++ Dig 1/Assets/Scripts/Player/BoomerangCollision.cs
@@ -2,35 +2,31 @@ using UnityEngine;
 
 public class BoomerangColission : MonoBehaviour
 {
-    [SerializeField] float hitDirectionForce = 10f;
-    [SerializeField] float additionalForce = 5f;
+    [SerializeField] private float hitDirectionForce = 10f;
+    [SerializeField] private float additionalForce   = 5f;
     // Script references
-    PlayerCombat playerCombat;
+    private PlayerCombat playerCombat;
 
-    void Awake()
+    private void Awake()
     {
         playerCombat = FindAnyObjectByType<PlayerCombat>();
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            EnemyHealth enemyHealth;
-
-            GameObject enemy = collision.gameObject;
-            Vector2 direction = (enemy.transform.position - transform.position).normalized;
-            enemyHealth = enemy.gameObject.GetComponentInChildren<EnemyHealth>();
+	        var  enemy       = collision.gameObject;
+            Vector2     direction   = (enemy.transform.position - transform.position).normalized;
+            var enemyHealth = enemy.gameObject.GetComponentInChildren<EnemyHealth>();
             
             Debug.Log("Enemy hit");
             enemyHealth.ChangeHealth(-playerCombat.GetBoomerangDamage(), direction, hitDirectionForce, additionalForce, collision.GetContact(0).point);
             playerCombat.GetEarlyReceiving(true);
         }
 
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            Debug.Log("Ground hit");
-            playerCombat.GetEarlyReceiving(true);
-        }
+        if (!collision.gameObject.CompareTag("Ground")) return;
+        Debug.Log("Ground hit");
+        playerCombat.GetEarlyReceiving(true);
     }
 }
