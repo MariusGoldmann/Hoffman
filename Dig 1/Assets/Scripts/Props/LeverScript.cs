05bdfe6450dadd1a72cec9d2c9338438574f1edd
Raymondaderman <raymond.aderman@gmail.com>
2026-05-12 07:45:04 +0200

    Foor stuff

--- Dig 1/Assets/Scripts/Props/LeverScript.cs
+++ Dig 1/Assets/Scripts/Props/LeverScript.cs
@@ -3,11 +3,14 @@ using UnityEngine.InputSystem;
 
 public class LeverScript : MonoBehaviour {
 	private static readonly  int      Open = Animator.StringToHash("Open");
+	private static readonly  int      Pull = Animator.StringToHash("Pull");
 	[SerializeField] private Animator doorAnimator;
+	[SerializeField] private Animator leverAnimator;
 
 	private void OnTriggerStay2D(Collider2D other) {
 		if (!Keyboard.current.eKey.wasPressedThisFrame || !other.CompareTag("Player")) return;
 		doorAnimator.SetTrigger(Open);
+		leverAnimator.SetTrigger(Pull);
 		Debug.Log("Open");
 	}
 }
